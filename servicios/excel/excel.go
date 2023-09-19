c3717af64a2f2375823098e8b7ffd4a4e8e330c0
ariadna <agonzalez@itpatagonia.com>
2023-09-19 14:28:49 -0300

    arreglos filtro

--- servicios/excel/excel.go
+++ servicios/excel/excel.go
@@ -98,7 +98,7 @@ func datosExcel(novedadesArr []novedades.Novedades, fechaDesde string, fechaHast
 		var pasosWorkflow novedades.PasosWorkflow
 		coll := client.Database(constantes.Database).Collection(constantes.CollectionPasosWorkflow)
 		err := coll.FindOne(context.TODO(), bson.M{"tipo": item.Descripcion}).Decode(&pasosWorkflow)
-		
+
 		if pasosWorkflow.TipoExcel == constantes.DescAnticipo {
 			err = anticipoPrestamo(file, item, rowGeneral, 1)
 			if err == nil {
@@ -353,9 +353,31 @@ func pagos(file *excelize.File, novedad novedades.Novedades, row *int) error {
 
 func allNovedades(file *excelize.File, novedad novedades.Novedades, row int) error {
 	err, recurso := recursos.GetRecursoInterno(novedad.Usuario, 0, 0)
+
+	coll := client.Database(constantes.Database).Collection(constantes.CollectionNovedad)
+	filter := bson.D{{Key: "Periodo", Value: novedad.Periodo}}
+
+	cursor, err := coll.Find(context.TODO(), filter)
+
+	//coll := client.Database(constantes.Database).Collection(constantes.CollectionNovedad)
+	//periodo := bson.D{{Key: "periodo", Value: bson.M{"$ne": novedad.Periodo}}}
+	//filter = bson.M{"$and": bson.A{periodo}}
+
+	//cursor, err := coll.Find(context.TODO(), filter)
+
+	//fecha := bson.D{{Key: "fechaDesde", Value: bson.M{"$ne": novedad.FechaDesde}}, {Key: "fechaHasta", Value: bson.M{"$ne": novedad.FechaHasta}}}
+	//filterR := bson.M{"$and": bson.A{fecha}}
+
+	//	cursor, err = coll.Find(context.TODO(), filterR)
+
+	var novedades []novedades.Novedades
+	if err = cursor.All(context.Background(), &novedades); err != nil {
+	}
+
 	if err != nil {
 		return err
 	}
+
 	file.SetCellValue(constantes.PestanaNovedades, fmt.Sprintf("A%d", row), novedad.Fecha)
 	file.SetCellValue(constantes.PestanaNovedades, fmt.Sprintf("B%d", row), novedad.IdSecuencial)
 	file.SetCellValue(constantes.PestanaNovedades, fmt.Sprintf("C%d", row), novedad.Descripcion)
@@ -492,7 +514,7 @@ func GetExcelPP(c *fiber.Ctx) error {
 	var novedades []novedades.Novedades
 
 	cursor, err := coll.Find(context.TODO(), bson.M{})
-	
+
 	if err = cursor.All(context.Background(), &novedades); err != nil {
 		return c.Status(fiber.StatusServiceUnavailable).SendString(err.Error())
 	}
@@ -524,41 +546,41 @@ func ExcelPP(novedadesArr []novedades.Novedades, fechaDesde string, fechaHasta s
 		err := coll.FindOne(context.TODO(), bson.M{"tipo": item.Descripcion}).Decode(&pasosWorkflow)
 		err = pagoProveedores(file, item, rowPagoProveedores)
 		if err == nil {
-			rowPagoProveedores ++
+			rowPagoProveedores++
 		}
-	}	
-			// guardar archivo
-		if err := file.SaveAs(os.Getenv("EXCELPP_FILE")); err != nil {
+	}
+	// guardar archivo
+	if err := file.SaveAs(os.Getenv("EXCELPP_FILE")); err != nil {
 		log.Printf("No se pudo guardar el archivo de Excel: %s", err.Error())
 		return err
 	}
-		return nil
-		
+	return nil
+
 }
 
 func pagoProveedores(file *excelize.File, novedad novedades.Novedades, row int) error {
-    proveedor, err := proveedores.ObtenerProveedores(0, 0, novedad.Proveedor)
-    if err != nil {
-        // Maneja el error si es necesario
-        return err
-    }
+	proveedor, err := proveedores.ObtenerProveedores(0, 0, novedad.Proveedor)
+	if err != nil {
+		// Maneja el error si es necesario
+		return err
+	}
 
-    cellMappings := map[string]interface{}{
-        fmt.Sprintf("A%d", row): novedad.Fecha,
-        fmt.Sprintf("B%d", row): novedad.IdSecuencial,
-        fmt.Sprintf("C%d", row): novedad.Estado,
-        fmt.Sprintf("D%d", row): novedad.Proveedor,
-        fmt.Sprintf("E%d", row): proveedor.CodProv,
-        fmt.Sprintf("F%d", row): proveedor.RazonSocial,
-        fmt.Sprintf("G%d", row): novedad.ImporteTotal,
-        fmt.Sprintf("H%d", row): novedad.Usuario,
-        fmt.Sprintf("I%d", row): novedad.Comentarios,
-        fmt.Sprintf("J%d", row): novedad.Periodo,
-    }
+	cellMappings := map[string]interface{}{
+		fmt.Sprintf("A%d", row): novedad.Fecha,
+		fmt.Sprintf("B%d", row): novedad.IdSecuencial,
+		fmt.Sprintf("C%d", row): novedad.Estado,
+		fmt.Sprintf("D%d", row): novedad.Proveedor,
+		fmt.Sprintf("E%d", row): proveedor.CodProv,
+		fmt.Sprintf("F%d", row): proveedor.RazonSocial,
+		fmt.Sprintf("G%d", row): novedad.ImporteTotal,
+		fmt.Sprintf("H%d", row): novedad.Usuario,
+		fmt.Sprintf("I%d", row): novedad.Comentarios,
+		fmt.Sprintf("J%d", row): novedad.Periodo,
+	}
 
-    for cell, value := range cellMappings {
-        file.SetCellValue(constantes.PestanaPagoProvedores, cell, value)
-    }
+	for cell, value := range cellMappings {
+		file.SetCellValue(constantes.PestanaPagoProvedores, cell, value)
+	}
 
-    return nil
+	return nil
 }
