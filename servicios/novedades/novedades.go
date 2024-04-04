a4fbdddb9238f48ba0506dcd7fca1d42154304e2
agonzalez <agonzalez@itpatagonia.com>
2024-04-04 16:23:05 -0300

    cambio importeTotal = float64

--- servicios/novedades/novedades.go
+++ servicios/novedades/novedades.go
@@ -47,7 +47,7 @@ type Novedades struct {
 	Freelance             bool                `bson:"freelance"`
 	Hora                  string              `bson:"hora"`
 	IdSecuencial          int                 `bson:"idSecuencial"`
-	ImporteTotal          int                 `bson:"importeTotal,truncate"`
+	ImporteTotal          float64             `bson:"importeTotal,truncate"`
 	Motivo                string              `bson:"motivo"`
 	OrdenDeCompra         string              `bson:"ordenDeCompra"`
 	Periodo               string              `bson:"periodo"`
